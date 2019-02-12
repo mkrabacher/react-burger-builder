3843307b865843c5ecc46c393bfb63da7ac393d7
Matt Krabacher <matt.krabacher@gmail.com>
2019-02-12 09:02:37 -0800

    created burger ingrediant component

---
+++ src/components/Burger/BurgerIngrediant/BurgerIngrediant.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './BurgerIngrediant.scss'
+
+
+const burgerIngrediant = (props) => {
+  let ingrediant = null; 
+
+  switch (props.type) {
+    case ('bread-bottom'):
+      ingrediant = <div className={'BreadBottom'}></div>
+      break;
+    case ('bread-top'):
+      ingrediant = (
+        <div className={'BreadTop'}>
+          <div className="Seeds1"></div>
+          <div className="Seeds2"></div>
+        </div>
+      );
+      break;
+    case ('meat'):
+      ingrediant = <div className={'Meat'}></div>
+      break;
+    case ('cheese'):
+      ingrediant = <div className={'Cheese'}></div>
+      break;
+    case ('lettuce'):
+      ingrediant = <div className={'Lettuce'}></div>
+      break;
+    case ('bacon'):
+      ingrediant = <div className={'Bacon'}></div>
+      break;
+  }
+
+  render() {
+    return ();
+  }
+}
+
+export default burgerIngrediant;
