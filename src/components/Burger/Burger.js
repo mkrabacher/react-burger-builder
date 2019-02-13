818e8f780fb29e043cefa4a272200d3035acd9d0
Matt Krabacher <matt.krabacher@gmail.com>
2019-02-13 09:16:34 -0800

    handled empty burger use case

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,11 +7,18 @@ const burger = (props) => {
 
   let fixins = [];
   fixins = Object.keys(props.fixins).map(fixinKey => {
-   return  [...Array(props.fixins[fixinKey])].map((_, i) => {
-      return <BurgerIngrediant key={fixinKey + i} type={fixinKey}/>;
+    return [...Array(props.fixins[fixinKey])].map((_, i) => {
+      return <BurgerIngrediant key={fixinKey + i} type={fixinKey} />;
     })
-    
   });
+  fixins = fixins.reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+  
+  if (fixins.length === 0) {
+    fixins = <p>Let's make a burger</p>
+  }
+
 
   return (
     <div className="Burger">
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,12 +5,12 @@ import Aux from '../../hoc/AuxWrapper'
 class BurgerBuilder extends Component {
   state = {
     fixins: {
-      'bread-top': 2,
-      'lettuce': 2,
-      'bacon': 3,
-      'cheese': 7,
+      'bread-top': 0,
+      'lettuce': 0,
+      'bacon': 0,
+      'cheese': 0,
       'meat': 0,
-      'bread-bottom': 1,
+      'bread-bottom': 0,
     }
   }
   
