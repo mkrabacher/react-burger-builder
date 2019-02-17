e523419c51ea371ed5aafffa70d370790c87484a
Matt Krabacher <matt.krabacher@gmail.com>
2019-02-17 14:05:11 -0800

    built and implemented controls array for each ingrediant

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,8 @@ import './BuildControl.scss';
 
 const buildControl = (props) => (
   <div className='BuildControl'>
-    <div className='Label'>{props.label}</div>
     <button className='Less'>Less</button>
+    <div className='Label'>{props.label}</div>
     <button className='More'>More</button>
   </div>
 );
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import BuildControl from './BuildControl/BuildControl'
+import './BuildControls.scss'
+
+const controls = [
+  { label: 'Top Bun', type: 'bread-top' },
+  { label: 'Lettuce', type: 'lettuce' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+  { label: 'Bottom Bun', type: 'bread-bottom' },
+]
 
 const buildControls = (props) => (
-  <div>
-    <BuildControl />
+  <div className='BuildControls'>
+    {controls.map(fixin => {
+      return <BuildControl key={fixin.label} label={fixin.label}/>
+    })}
   </div>
 
 )
