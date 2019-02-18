00c021eaedc91d4d2b2c75454144a2690d760993
Matt Krabacher <matt.krabacher@gmail.com>
2019-02-18 08:13:37 -0800

    Built and added Order Summary component

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Aux from '../../../hoc/AuxWrapper';
+
+const orderSummary = (props) => {
+  const fixins = Object.keys(props.fixins)
+    .map(key => {
+      return <li key={key}><span stlye={{textTransform: 'capitalize'}}>{key}</span>: {props.fixins[key]}</li>
+    })
+
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <ul>
+        {fixins}
+      </ul>
+      <p>Contrinue to Checkout?</p>
+    </Aux>
+  )
+};
+
+export default orderSummary;
