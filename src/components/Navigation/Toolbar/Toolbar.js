45341c2a84ddc915cd3a81846681b6ca68336524
Matt Krabacher <matt.krabacher@gmail.com>
2019-02-18 15:28:13 -0800

    created toolbar component with basic styling

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Toolbar.scss';
+
+const toolbar = (props) => (
+  <header className='Toolbar'>
+    <div>Menu</div>
+    <div>Logo</div>
+    <nav>
+      ...
+    </nav>
+  </header>
+);
+
+export default toolbar;
