9b4cab0363a7edcd0710bf1d00cef11d70714998
Matt Krabacher <matt.krabacher@gmail.com>
2019-03-03 12:43:03 -0800

    turned Modal into stateful component to control when render happens

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Modal.scss';
 import Backdrop from '../Backdrop/Backdrop';
 import Aux from '../../../hoc/AuxWrapper';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+   return (<Aux>
+    <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
     <div
     className='Modal'
       style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0', 
+        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: this.props.show ? '1' : '0', 
       }}>
-      {props.children}
+      {this.props.children}
     </div>
-  </Aux>
-)
+  </Aux>)
+  }
+} 
 
-export default modal; 
+export default Modal; 
